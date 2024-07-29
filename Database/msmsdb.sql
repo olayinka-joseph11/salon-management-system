aa2502325edf9aba99db17f86feadd36557577d8
covenant123456 <covenantekundayo@gmail.com>
2024-07-29 02:03:39 +0100

    first commit

---
+++ Database/msmsdb.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2023 at 01:42 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `msmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` char(50) DEFAULT NULL,
+  `UserName` char(50) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 9365524166, 'tester1@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '2019-07-25 06:21:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblappointment`
+--
+
+CREATE TABLE `tblappointment` (
+  `ID` int(10) NOT NULL,
+  `AptNumber` varchar(80) DEFAULT NULL,
+  `Name` varchar(120) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `PhoneNumber` bigint(11) DEFAULT NULL,
+  `AptDate` varchar(120) DEFAULT NULL,
+  `AptTime` varchar(120) DEFAULT NULL,
+  `Services` varchar(120) DEFAULT NULL,
+  `ApplyDate` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(250) NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `RemarkDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcustomers`
+--
+
+CREATE TABLE `tblcustomers` (
+  `ID` int(10) NOT NULL,
+  `Name` varchar(120) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(11) DEFAULT NULL,
+  `AptNumber` varchar(120) DEFAULT NULL,
+  `Gender` enum('Female','Male','Transgender') DEFAULT NULL,
+  `app_date` varchar(120) DEFAULT NULL,
+  `app_time` varchar(120) DEFAULT NULL,
+  `Details` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcustomers`
+--
+
+INSERT INTO `tblcustomers` (`ID`, `Name`, `Email`, `MobileNumber`, `AptNumber`, `Gender`, `app_date`, `app_time`, `Details`, `CreationDate`) VALUES
+(42, 'Abhishek Singh', 'abhisheksingh81037272@gmail.com', 8237283823, '341188319', 'Male', '2023-04-25', '16:38', 'dadad', '2023-04-25 11:08:09'),
+(43, 'Nayan', 'dmnd@gmail.com', 8237283823, '777534318', 'Transgender', '2023-05-06', '05:41', 'hello', '2023-04-25 11:10:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblinvoice`
+--
+
+CREATE TABLE `tblinvoice` (
+  `id` int(11) NOT NULL,
+  `Userid` int(11) DEFAULT NULL,
+  `ServiceId` int(11) DEFAULT NULL,
+  `BillingId` int(11) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblinvoice`
+--
+
+INSERT INTO `tblinvoice` (`id`, `Userid`, `ServiceId`, `BillingId`, `PostingDate`) VALUES
+(43, 37, 8, 165333293, '2023-04-25 08:15:53'),
+(44, 38, 2, 162292257, '2023-04-25 08:17:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpage`
+--
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` mediumtext DEFAULT NULL,
+  `PageDescription` mediumtext DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` date DEFAULT NULL,
+  `Timing` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpage`
+--
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`, `Timing`) VALUES
+(1, 'aboutus', 'About Us', '                                                                                                        Our main focus is on quality and hygiene. Our Parlour is well equipped with advanced technology equipments and provides best quality services. Our staff is well trained and experienced, offering advanced services in Skin, Hair and Body Shaping that will provide you with a luxurious experience that leave you feeling relaxed and stress free. The specialities in the parlour are, apart from regular bleachings and Facials, many types of hairstyles, Bridal and cine make-up and different types of Facials &amp; fashion hair colourings.quality services. Our staff is well trained and experienced, offering advanced services in Skin, Hair and Body Shaping that will provide you with a luxurious experience that leave you feeling relaxed and stress free. The specialities in the parlour are, apart.colourings.quality services. Our staff is well trained and experienced, offering advanced services in Skin, Hair and Body Shaping that will provide you with a luxurious experience that leave you feeling relaxed and stress free. The specialities in the parlour are, apart.colourings.quality services. Our staff is well trained and experienced, offering advanced services in Skin, Hair and Body Shaping that will provide you.', NULL, NULL, NULL, ''),
+(2, 'contactus', 'Contact Us', 'Nerim, Assam', 'Abhishek@gmail.com', 9365524166, NULL, '10:30 am to 8:30 pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblservices`
+--
+
+CREATE TABLE `tblservices` (
+  `ID` int(10) NOT NULL,
+  `ServiceName` varchar(200) DEFAULT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `Cost` int(10) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblservices`
+--
+
+INSERT INTO `tblservices` (`ID`, `ServiceName`, `Description`, `Cost`, `CreationDate`) VALUES
+(2, 'Fruit Facial', 'If its a peel-off mask, it also works as an excellent exfoliator, ridding the skin of dead cells.', 500, '2019-07-25 11:22:53'),
+(3, 'Charcol Facial', 'The end result is skin that is clean and clear. When used as a powder, charcoal masks can reach deep in your pores and suck out impurities with them.', 1000, '2019-07-25 11:23:10'),
+(4, 'Deluxe Menicure', 'The end result is skin that is clean and clear. When used as a powder, charcoal masks can reach deep in your pores and suck out impurities with them.', 500, '2019-07-25 11:23:34'),
+(5, 'Deluxe Pedicure', 'A pedicure is a therapeutic treatment for your feet that removes dead skin, softens hard skin and shapes and treats your toenails.', 600, '2019-07-25 11:23:47'),
+(6, 'Normal Menicure', 'A ped
